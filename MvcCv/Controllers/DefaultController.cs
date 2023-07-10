8c6625b6d4bec9c7a55fa32cf5f973cace52cff2
Batuhan Aydın <batuhanaydn2034@gmail.com>
2023-07-10 11:50:21 +0300

    eğitimlerim,sertifiklarım,Yeteneklerim,iletişim kısımlarını tamamladım

--- MvcCv/Controllers/DefaultController.cs
+++ MvcCv/Controllers/DefaultController.cs
@@ -22,5 +22,43 @@ namespace MvcCv.Controllers
             var deneyimler = db.TblDeneyimlerim.ToList();
             return PartialView(deneyimler);
         }
+
+        public PartialViewResult egitimlerim()
+        {
+            var egitimlerim = db.TblEgitimlerim.ToList();
+            return PartialView(egitimlerim);
+        }
+
+        public PartialViewResult Yeteneklerim()
+        {
+            var Yetenekler = db.TblYeteneklerim.ToList();
+            return PartialView(Yetenekler);
+        }
+
+        public PartialViewResult Hobilerim()
+        {
+            var hobiler = db.TblHobilerim.ToList();
+            return PartialView(hobiler);
+        }
+
+        public PartialViewResult Sertifikalarım()
+        {
+            var Sertifika = db.TblSertifikalarim.ToList();
+            return PartialView(Sertifika);
+        }
+
+        [HttpGet]
+        public PartialViewResult İletisim()
+        {
+
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult İletisim(Tblİletişim t)
+        {
+            db.Tblİletişim.Add(t);
+            db.SaveChanges();
+            return PartialView();
+        }
     }
 }
